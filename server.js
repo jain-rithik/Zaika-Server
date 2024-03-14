324b95e4cd5a61b2ed4d0a4f48ba07df6e823d91
Rithik <jainrithik2000@gmail.com>
2024-03-14 14:50:54 +0530

    Server created

--- server.js
+++ server.js
@@ -9,10 +9,41 @@ app.use(cors());
 
 // For Restaurant API
 app.get('/api/restaurants', async (req, res) => {
-    const { lat, lng, page_type } = req.query;
+    const { lat, lng } = req.query;
     console.log(req.query);
 
-    const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&page_type=${page_type}`;
+    const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&page_type=DESKTOP_WEB_LISTING`;
+
+    await fetch(url, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            res.json(data);
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).send('An error occurred');
+        });
+});
+
+// For Restaurant Mobile API
+app.get('/mapi/restaurants', async (req, res) => {
+    const { lat, lng } = req.query;
+    console.log(req.query);
+
+    const url = `https://www.swiggy.com/mapi/homepage/getCards?lat=${lat}&lng=${lng}`;
 
     await fetch(url, {
         headers: {
@@ -41,10 +72,10 @@ app.get('/api/restaurants', async (req, res) => {
 
 // For Menu API
 app.get('/api/menu', async (req, res) => {
-    const { 'page-type': page_type, 'complete-menu': complete_menu, lat, lng, submitAction, restaurantId } = req.query;
+    const { lat, lng, restaurantId } = req.query;
     console.log(req.query);
 
-    const url = `https://www.swiggy.com/dapi/menu/pl?page-type=${page_type}&complete-menu=${complete_menu}&lat=${lat}&lng=${lng}&submitAction=${submitAction}&restaurantId=${restaurantId}`;
+    const url = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=${lat}&lng=${lng}&restaurantId=${restaurantId}`;
 
 
     await fetch(url, {
@@ -73,7 +104,7 @@ app.get('/api/menu', async (req, res) => {
 
 
 app.get('/', (req, res) => {
-    res.json({ "test": "Welcome to FoodFire! - See Live Web URL for this Server - https://foodfire-app.netlify.app" });
+    res.json({ "test": "Welcome to Zaika! - See Live Web URL for this Server - https://zaika-foods.netlify.app/" });
 })
 
 app.listen(port, () => {
